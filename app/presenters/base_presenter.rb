6187e306f8140253e0fb3706fb36c0dcddb058ed
Rafał Rybicki <rafarybicki@gmail.com>
2020-01-22 15:51:19 +0100

    Add Paginator

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,20 +1,56 @@
 class BasePresenter
+# metaprograming below
+  # CLASS_ATTRIBUTES = {
+  #   build_with: :build_attributes,
+  #   related_to: :relations,
+  #   sort_by: :sort_attributes,
+  #   filter_by: :filter_attributes
+  # }
+  #
+  # CLASS_ATTRIBUTES.each { |k, v| instance_variable_set("@#{v}", []) }
+  #
+  # class << self
+  #   attr_accessor *CLASS_ATTRIBUTES.values
+  #
+  #   CLASS_ATTRIBUTES.each do |k, v|
+  #     define_method k do |*args|
+  #       instance_variable_set("@#{v}", args.map(&:to_s))
+  #     end
+  #   end
+  # end
+
+
   # Define a class level instance variable
+  @relations = []
+  @sort_attributes = []
+  @filter_attributes = []
   @build_attributes = []
 
   # Open the door to class methods
   class << self
-    # Define an accessor for the class level instance
-    # variable we created above
-    attr_accessor :build_attributes
+    # Define the accessors for the attributes created
+    # above
+    attr_accessor :relations, :sort_attributes,
+                  :filter_attributes, :build_attributes
 
-    # Create the actual class method that will
-    # be used in the subclasses
-    # We use the splash operation '*' to get all
-    # the arguments passed in an array
     def build_with(*args)
       @build_attributes = args.map(&:to_s)
     end
+
+    # Add a bunch of methods that will be used in the
+    # model presenters
+    def related_to(*args)
+      @relations = args.map(&:to_s)
+    end
+
+    def sort_by(*args)
+      @sort_attributes = args.map(&:to_s)
+    end
+
+    def filter_by(*args)
+      @filter_attributes = args.map(&:to_s)
+    end
+
   end
 
   attr_accessor :object, :params, :data
